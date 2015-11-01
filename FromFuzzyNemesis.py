1f21026221c416513229e65d447cace6aa39abe9
crimedude22 <jsaunder@willamette.edu>
2015-11-01 01:42:36 -0800

    First

---
+++ FromFuzzyNemesis.py
@@ -0,0 +1,235 @@
+__author__ = 'Jonny'
+#!/usr/bin/python
+import pattern.web
+import re, sys, os
+from operator import itemgetter
+from pattern.web import plaintext, DOM
+from bs4 import BeautifulSoup
+from collections import Counter
+import pattern.vector as vec
+
+rap_exclude_words = ['1','2', '3', 'x2', 'ay', 'hey', 'uhh', '\'mon', 'cuz', 'c', '\'mma', 'kanye', 'west', 'yo', 't', 'oh', 'uh', 'ya', 'yea', 'la', 'gon', 'cause', 'em', 'yeah', '50', 'cent']
+
+# ok, just write a thing that opens the directory with all the artists and prints out the directory
+# names
+
+basedir = "/Users/Jonny/Downloads/RapGenius/"
+common_words_file = "/Users/Jonny/Documents/Scripting/RapResults/Results"
+results_basedir = "/Users/Jonny/Documents/Scripting/RapResults/"
+stop_names_file = "/Users/Jonny/Documents/Scripting/RapResults/stop_names"
+
+def get_common_words(file):
+    common = []
+    f = open(file)
+    for line in f:
+        common.append(re.split("\s", line)[1].strip())
+    return common
+
+# put all the rapper names into a file
+def make_exclude_names():
+    f = open(stop_names_file, 'w')
+
+    for dir in os.listdir(basedir):
+        # dir names are the name of the artist, separated by -, like jay-z
+        name1 = re.split('-', dir)
+        for n in name1:
+            f.write(n)
+            f.write("\n")
+            print n
+    f.close()
+
+def load_exclude_names():
+    global rap_exclude_words
+    f = open(stop_names_file)
+    for line in f:
+        rap_exclude_words.append(line.strip())
+
+
+def get_artist_docs(name):
+
+    default_dir = basedir + name
+    rap_docs = ""
+
+    # get a list of all the files in default dir
+
+    for f in os.listdir(default_dir):
+        print f
+        # go to that dir
+        os.chdir(default_dir)
+        # open the file
+        fi = open(f, 'r')
+        # print "reading " + f
+        # slurp
+        page = fi.read()
+
+        # what does this do?
+        dom = DOM(page)
+
+        # we look at the page and get that the thing we want is in the .lyrics div.
+        if dom and dom('.lyrics'):
+            lyrics =  dom('.lyrics')[0]
+        else:
+            continue
+
+        p = plaintext(lyrics.content)
+        rap_docs += p
+
+    return rap_docs
+
+    # . questions we have, most common 20 words over ALL artists
+    # . what are words that appear only for that artist? like mad for biggie or flavor for queen latifah
+    # . what are words that appear only for eminem or biggie small?
+
+    # import pdb; pdb.set_trace()
+    # Kanye_corpus = vec.M(rap_docs, weight=vec.TFIDF)
+
+# pass in a dict of bad words, return a dict of word counts
+
+def count_one_artist(name, bad_words):
+
+    # ok, this is a bad way to get number of songs for that artist, so we can average out
+    # the words per song
+    default_dir = basedir + name
+    num_songs = len(os.listdir(default_dir))
+
+    # we need the number of songs, this is so annoying
+    dict = {}
+    docs = vec.count(vec.words(get_artist_docs(name)))
+    for w in bad_words:
+        if w in docs:
+            dict[w] = docs[w]
+    dict['num_songs'] = num_songs # this is cheap
+    return dict
+
+
+def get_one_artist_excluding_common(name):
+
+    global rap_exclude_words
+
+    common_exclude_words = get_common_words(common_words_file)
+    rap_exclude_words += common_exclude_words
+    # ok this is the worse way to do this
+    load_exclude_names()
+
+    docs = get_artist_docs(name)
+    corpus = vec.Document(docs, exclude=rap_exclude_words, stop_words=False)
+    print "for artist: " + name
+    for ln in corpus.keywords(top=20): print "%0.08f\t%s" % ln
+
+
+def get_each_artist_excluding_common():
+    global rap_exclude_words
+    common_exclude_words = get_common_words(common_words_file)
+    rap_exclude_words += common_exclude_words
+
+    for dir in os.listdir(basedir):
+        if dir != '.git':
+            if dir != ".DS_Store":
+        # push the name of the artist onto the exclude words list
+                name1  = re.split('-', dir)
+                for n in name1:
+                    rap_exclude_words.append(n)
+
+                docs = get_artist_docs(dir)
+                results_file = results_basedir + dir
+                f = open(results_file, 'w')
+                corpus = vec.Document(docs, exclude=rap_exclude_words, stop_words=False)
+                f.write("for artist: " + dir)
+                print "for artist: " + dir
+                f.write("\n")
+                #for ln in corpus.keywords(top=20):
+                #    f.write("%0.08f\t%s" % ln)
+                #    print "%0.08f\t%s" % ln
+                z = docs.encode('ascii',errors='ignore')
+                f.write(z)
+                f.close()
+
+get_each_artist_excluding_common()
+
+
+# print the results for the top 20 words to a file
+
+def get_one_artist_all_words(name):
+    docs = ""
+    name1  = re.split('-', name)
+    for n in name1:
+        rap_exclude_words.append(n)
+        print "excluding" + n
+    docs = get_artist_docs(name)
+    corpus = vec.Document(docs, exclude=rap_exclude_words, stop_words=False)
+    for ln in corpus.keywords(top=20): print "%0.08f\t%s" % ln
+
+def get_all_artists_all_words():
+    docs = ""
+    for dir in os.listdir(basedir):
+        print "artist" + dir
+        if dir != '.git':
+        # push the name of the artist onto the exclude words list
+            name1  = re.split('-', dir)
+            for n in name1:
+                rap_exclude_words.append(n)
+            # import pdb; pdb.set_trace()
+            docs += get_artist_docs(dir)
+
+    corpus = vec.Document(docs, exclude=rap_exclude_words, stop_words=False)
+    for ln in corpus.keywords(top=20): print "%0.08f\t%s" % ln
+
+def get_all_artists_all_words_to_file():
+    f = open(common_words_file, "w")
+    docs = ""
+
+    for dir in os.listdir(basedir):
+        if dir != ".DS_Store":
+            print "artist" + dir
+            if dir != '.git':
+            # push the name of the artist onto the exclude words list
+                name1  = re.split('-', dir)
+                for n in name1:
+                    rap_exclude_words.append(n)
+
+                docs += get_artist_docs(dir)
+
+    corpus = vec.Document(docs, exclude=rap_exclude_words, stop_words=False)
+    for ln in corpus.keywords(top=20):
+        f.write("%0.08f\t%s" % ln)
+        print "%0.08f\t%s" % ln
+    f.close()
+
+#get_all_artists_all_words_to_file()
+
+if __name__ == '__main__':
+
+    if sys.argv[1:]:
+        name = sys.argv[1]
+
+##################################
+###################################
+def make_song_list(artist_name):
+    allfiles = []
+    dir = basedir + artist_name
+    filenames = os.listdir(dir)
+    for f in filenames:
+            if re.search( '\.git', f):
+                continue
+            else:
+                allfiles.append(os.path.join(dir,f))
+    return allfiles
+
+def scrape_info_from_song(file):
+    f = open(file, 'r')
+    page = f.read()
+    # using beautiful soup to navigate the dom and get
+    # back the stuff that we need
+    soup = BeautifulSoup(page)
+    soup.prettify()
+    lyrics = soup.select(".lyrics")
+    if lyrics:
+        lyrics_string = lyrics[0].text
+        lyrics_string = re.sub("\n", " ", lyrics_string)
+        return lyrics_string
+    else:
+        return ""
+
+artisty = []
+for artists in soup.find_all(href=re.compile("artist")):
+    art = artists.string
---
+++ InfoExtract.py
@@ -0,0 +1,126 @@
+__author__ = 'Jonny'
+import pattern.web
+import re, sys, os
+from os import walk
+from operator import itemgetter
+from pattern.web import plaintext, DOM
+from bs4 import BeautifulSoup
+from collections import Counter
+import pattern.vector as vec
+import os
+import progressbar
+import csv
+import pickle
+import json
+
+basedir = "/Users/Jonny/Downloads/RapGenius/"
+common_words_file = "/Users/Jonny/Documents/Scripting/RapResults/Results"
+results_basedir = "/Users/Jonny/Documents/Scripting/RapResults/"
+stop_names_file = "/Users/Jonny/Documents/Scripting/RapResults/stop_names"
+artist_names_file = "/Users/Jonny/Documents/Scripting/RapResults/indexes/artist_names"
+lyrics_index_dir = "/Users/Jonny/Documents/Scripting/RapResults/IndexedLyrics/"
+indexes_dir = "/Users/Jonny/Documents/Scripting/RapResults/indexes/"
+
+def get_artist_names(dir = basedir):
+    f = open(artist_names_file, 'w')
+
+    for dir in os.listdir(basedir):
+        # dir names are the name of the artist, separated by -, like jay-z
+        name1 = re.split('-', dir)
+        for n in name1:
+            f.write(n)
+            f.write("\n")
+            print n
+    f.close()
+
+
+def make_song_list(artist_name):
+    allfiles = []
+    dir = basedir + artist_name
+    filenames = os.listdir(dir)
+    for f in filenames:
+            if re.search( '\.git', f):
+                continue
+            else:
+                allfiles.append(os.path.join(dir,f))
+    return allfiles
+
+def get_files(dir):
+    global filenames
+    filenames = []
+    for root, dirs, files in os.walk(dir, topdown=False):
+        for name in files:
+            filenames[len(filenames):] = [os.path.join(root, name)]
+
+def extract_song_info(dir):
+    global filenames
+    get_files(dir)
+    #pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval=len(filenames)).start()
+    idcount = 0 #to assign unique id to each lyrics file
+    print len(filenames) #to keep track of function progress... inelegantly...
+    fileid = dict() #two dictionaries to move back and forth between ids and filenames
+    idfile = dict()
+    artistsongid = dict() #"" between artists and ids
+    songidartist = dict()
+    for i in filenames:
+        art = list()
+        print idcount #"progress bar"
+        fileid[i] = idcount
+        idfile[idcount] = [i]
+        ##open and read file with beautifulsoup
+        f = open(i, 'r')
+        page = f.read()
+        soup = BeautifulSoup(page)
+        soup.prettify()
+        ##get artists from page and index them
+        for artists in soup.find_all(href=re.compile("artist")):
+            addart = artists.string
+            if addart:
+                if addart != "Verified Artists":
+                    art[len(art):] = [addart]
+                    songidartist[idcount] = art #index all artists for given song by id
+        for i in art:
+            if i in artistsongid.keys():
+                existlist = [artistsongid[i]]
+                newlist = [existlist[:], idcount]
+                artistsongid[i] = (newlist) #and vice versa
+            else:artistsongid[i] = idcount
+        ######################################
+        #failed attempts at saving the indexes
+        #pickle.dump(artistsongid, open(as_index_file, "wb"))
+        #pickle.dump(songidartist, open(sa_index_file, "wb"))
+        #aa = csv.writer(codecs.open(as_index_file, 'w', encoding='utf_8', errors='ignore'))
+        #bb = csv.writer(codecs.open(sa_index_file, 'w', encoding='utf_8', errors='ignore'))
+        #for key, val in artistsongid.items():
+        #    aa.writerow([key, val])
+        #    bb.writerow([val, key])
+        ########################################
+        #Extract lyrics as plain text and save as indexed file
+        lyrics = soup.select(".lyrics")
+        if lyrics:
+            lyrics_string = lyrics[0].text
+            lyrics_string = re.sub("\n", " ", lyrics_string)
+            lyrics_string = lyrics_string.encode('ascii',errors='ignore')
+            lyrics_file = lyrics_index_dir + str(idcount)
+            f = open(lyrics_file, 'w')
+            f.write(lyrics_string)
+            f.close()
+        else: ""
+        #pbar.update(idcount)
+        idcount = idcount + 1
+    #save indices
+    f = open(as_index_file, 'w')
+    as_index_file = indexes_dir + "ArtistSongID"
+    sa_index_file = indexes_dir + "SongIDArtist"
+    json.dump(artistsongid, f)
+    f.close()
+    f = open(sa_index_file, 'w')
+    json.dump(songidartist, f)
+    f.close()
+
+
+##save fileid and idfile dictionaries at the end too
+##clean lyrics of names
+##count words, exlude words
+##Lyrics in SongIndex# files
+##Artist:SongIndex, SongIndex:SongName
